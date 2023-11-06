7fd4a9ab0f41f36b8edc88e16a3d93fb51294860
AshlynChuang <11056038@ntub.edu.tw>
2023-11-06 20:07:55 +0800

    first commit

--- mysite/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mysite.models import Post, Product
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'pub_date')
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Product)
